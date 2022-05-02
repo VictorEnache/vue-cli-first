25b4dd6e18fb720a9e314a1d1ae19edbafbcf3af
VictorEnache <victor22enache@gmail.com>
2022-05-02 20:59:58 +0200

    La mia prima app con cli

--- hello_world/src/App.vue
+++ hello_world/src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Title_1 msg="La mia prima app con Vue CLI"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Title_1 from './components/Title.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Title_1
   }
 }
 </script>
@@ -19,8 +19,6 @@ export default {
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
---
+++ hello_world/src/components/Title.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="vue_cli">
+        <h1>La mia prima app con Vue CLI!</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Title_1',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+h1{
+    font-size: 5rem;
+    text-align: center;
+    color: red;
+}
+</style>
